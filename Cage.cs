323cce12a4167c4c325879f75cf561da57fd39dd
Thanh Dat <datmathoingu@gmail.com>
2020-09-22 00:16:05 +0700

    Initial commit

---
+++ Cage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zoo_management_system
+{
+    public class Cage
+    {
+        public int CageNumber { get; set; }
+        public List<Animal> AnimalList { get; set; }
+
+        public Cage()
+        {
+            AnimalList = new List<Animal>();
+        }
+
+        public void AddAnimal(Animal a)
+        {
+            AnimalList.Add(a);
+        }
+        public void RemoveAnimal(String Name)
+        {
+            for (int i = 0; i < AnimalList.Count; i++)
+            {
+                if (AnimalList[i].Name.Equals(Name))
+                {
+                    AnimalList.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zoo_management_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Add Cage");
+            Console.WriteLine("2.Remove Cage");
+            Console.WriteLine("3.Add Animal");
+            Console.WriteLine("4.Remove Animal");
+            Console.WriteLine("5.Iterate Animal");
+            Console.WriteLine("6.Exit");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1: break;
+            }
+        }
+    }
+}
